cadba84a832e52d3ae1db14684c8ac4f18b5bd17
Nickz <nicolasaraujo845@gmail.com>
2023-09-27 09:54:51 -0300

    criacao dos componentes layout/ titulo

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,7 +1,11 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return(
-    <div className="flex h-screen justify-center items-center">
-      <span className="text-4xl">Texto</span>
+    <div className="flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-500 text-white">
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
